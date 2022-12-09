50bf23248d09fd478d934aeecc323f66151e859b
Tere Pana <terepana7@gmail.com>
2022-12-09 13:40:13 -0300

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
                 
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portafolio</span>
-                        <h1>{"Hi I am Teresa Pana"} <br /> <span className="wrap"> Desarroladora Full-stack</span></h1>
+                        <h1>{"Hi I am Teresa Pana"} <br /> <span className="wrap">Full-stack Developer</span></h1>
                         <p>I'm a proactive person,
 excellent teammate
 organized and responsible. I enjoy
